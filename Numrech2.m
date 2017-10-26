e11ed97aa848b3902e5224c7c0463ac59a1bd314
Demonkeys <33100475+Demonkeys@users.noreply.github.com>
2017-10-26 12:13:38 +0200

    Update Numrech2.m

--- Numrech2.m
+++ Numrech2.m
@@ -6,18 +6,18 @@ p0 = sin(8*x);
 %Erster Grad
 p1 = 1+0*x;
 %Zweiter Grad
-p2 = 1+32*(x.^2-pi/8*x+pi.^2/16.^2);
+p2 = 1-32*(x.^2-pi/8*x+pi.^2/16.^2);
 %Plottet die drei Funktionen mit den Eingabewerten von x
 plot(x,p0,x,p1,x,p2);
 %Das verstehst du hoffentlich nigga
 title('Taylorpolynom');
-legend('sin(8x)','1','1+32*(x-pi/16)^2');
+legend('sin(8x)','1','1-32*(x-pi/16)^2');
 
 %Die letzte Aufgabe
 a = pi/32:pi/1000:3*pi/32;
 %Ich habe vergessen wie man diese Funktionen nennt, aber du verstehst das
 %sicher xD
-f0 = a*8.*tan(8*a);
+f0 = abs(a*8.*cotan(8*a));
 f1 = abs(sin(8*a)-sin(8*(a+0.05))/sin(8*a));
 %figure öffnet ein zweites Fenster, damit man einfach nur einen Code nutzen
 %kann. Ich gucke mal später nach, ob man den Titel ändern kann des Fensters
